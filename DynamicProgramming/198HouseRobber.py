8c28e9e2a6559baa225ddf9af7cfa7678d826754
lucasarano <larano3@gatech.edu>
2024-10-07 15:39:55 -0400

    198

---
+++ DynamicProgramming/198HouseRobber.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rob(self, nums: list[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) <= 2:
+            return max(nums[0], nums[1])
+        one = nums[0]
+        two = max(one, nums[1])
+        i = 2
+        money = 0
+        while i < len(nums):
+            money = max(nums[i] + one, two)
+            one = two
+            two = money
+            i += 1
+        return two
+    
